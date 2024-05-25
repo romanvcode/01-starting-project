9257d5fa814fea9e01976a8eb4583daf71147b28
romanvcode <roman.v.ua@gmail.com>
2024-05-25 12:39:53 +0300

    Refactor user list rendering and add fallback message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,10 +13,10 @@ import { DUMMY_USERS } from './dummy-users';
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId = 'u1';
+  selectedUserId?: string;
 
   get selectedUser() {
-    return this.users.find((user) => user.id === this.selectedUserId)!;
+    return this.users.find((user) => user.id === this.selectedUserId);
   }
 
   onSelectUser(id: string) {
